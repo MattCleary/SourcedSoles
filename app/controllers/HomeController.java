f5850ea01d84776dcf114d30bb0f2f78d1e7f0fe
Shane Cotter <Shane Cotter>
2016-12-08 14:59:23 +0000

    Add and delete working

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -13,6 +13,13 @@ import views.html.*;
 
 public class HomeController extends Controller {
 
+    private FormFactory formFactory;
+
+    @Inject
+    public HomeController(FormFactory f){
+        this.formFactory = f;
+    }
+
 
     public Result index() {
         return ok(index.render());
@@ -22,6 +29,40 @@ public class HomeController extends Controller {
 
         List<Product> productsList = Product.findAll();
         return ok(products.render(productsList));
+    }
+
+    public Result addProduct(){
+
+        Form<Product> addProductForm = formFactory.form(Product.class);
+
+      return ok(addProduct.render(addProductForm));
+    }
+
+    public Result addProductSubmit(){
+
+        Form<Product> newProductForm = formFactory.form(Product.class).bindFromRequest();
+
+        if(newProductForm.hasErrors()){
+
+            return badRequest(addProduct.render(newProductForm));
+        }
+
+        Product newProduct = newProductForm.get();
+
+        newProduct.save();
+
+        flash("success", "Product " + newProduct.getName() + " has been created");
+
+        return redirect(controllers.routes.HomeController.products());
+    }
+
+    public Result deleteProduct(Long id){
+
+        Product.find.ref(id).delete();
+
+        flash("success", "Product has been deleted");
+
+        return redirect(routes.HomeController.products());
     }
 
 }
